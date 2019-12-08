0d833f5ba40d1369cb86b1f0bcea832c3350ee40
argyro <argyrogounari@gmail.com>
2019-12-08 21:22:10 +0000

    Add user view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Volunteering Opportunities - @yield('title')</title>
+    </head>
+    <body>
+        <h1>Volunteering Opportunities - @yield('title')</h1>
+
+        <div>
+            @yield('content')
+        </div>
+    </body>
+</html>
