8764dcc775bd4e377ad65d4b8a7cf4b7856b42a1
argyro <argyrogounari@gmail.com>
2019-12-10 00:12:53 +0000

    Refactor User with default values to aid registration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->binary('profile_picture');
-            $table->string('country');
+            $table->string('profile_picture')->default('default_profile_picture.jpg');
+            $table->string('country')->default('Unknown');
             $table->rememberToken();
             $table->timestamps();
 
