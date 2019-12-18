3cec61d37f17a85ef7a8debbbe86c236590e5a00
argyro <argyrogounari@gmail.com>
2019-12-18 19:50:04 +0200

    Add basic post info view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,27 +1,118 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', 'Post details')
+<head>
 
-@section('content')
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-<p>The volunteering opportunitie:</p>
+  <title>Shop Item - Start Bootstrap Template</title>
 
-<ul>
-    <li>Title: {{ $post->title }}</li>
-    <li>Country: {{ $post->country }}</li>
-    <li>Start Date: {{ $post->start_date }}</li>
-    <li>End Date: {{ $post->end_date }}</li>
-    <li>Description: {{ $post->description }}</li>
-    <li>Apply: {{ $post->application_url }}</li>
-    <li>Organization: {{ $post->organization->user->name }}</li>
-</ul>
+  <!-- Bootstrap core CSS -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
 
-<form method="POST"
-    action="{{ route('posts.destroy', ['post' => $post]) }}">
-    @csrf
-    @method('DELETE')
-    <button type="submit">Delete</button>
-</form>
+  <!-- Custom styles for this template -->
+  <link href="../css/shop-item.css" rel="stylesheet">
 
-<a href="{{ route('posts.index') }}">Back</a>
-@endsection
+</head>
+
+<body>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-9">
+
+        <div class="card mt-4">
+          <img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
+          <div class="card-body">
+            <h3 class="card-title">{{ $post->title }}</h3>
+            <h4>Posted by: {{ $post->organization->user->name }}</h4>
+            <p class="card-text">{{ $post->description }}</p>
+          </div>
+        </div>
+        <!-- /.card -->
+
+        <div class="card card-outline-secondary my-4">
+          <div class="card-header">
+            Comments
+          </div>
+            <div class="card-body" id="root">
+                <ul v-for="comment in comments">
+                    <p>@{{ comment.comment_text }}</p>
+                    <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+                    <hr>
+                </ul>
+            <a href="#" class="btn btn-success">Leave a Comment</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="../jquery/jquery.min.js"></script>
+  <script src="../js/bootstrap.bundle.min.js"></script>
+
+  <script>
+    var app = new Vue({
+        el: "#root",
+        data: {
+            comments: [],
+        },
+        mounted() {
+            
+        },
+        methods: {
+          
+        }
+    });
+  </script>
+</body>
+
+</html>
