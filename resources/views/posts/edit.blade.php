70e66fd73b12248abbdee6bec925e5c7be27783b
argyro <argyrogounari@gmail.com>
2019-12-19 21:50:57 +0200

    Fix edit post access

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,31 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-@if (Auth::user()->id === $post->user_id)
+
 <div class="container">
     <div class="row">
       <div class="col-lg-9">
         <div class="card card-outline-secondary my-4">
           <div class="card-body" id="root">
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  @else
-    <div class="container">
-    <div class="row">
-      <div class="col-lg-9">
-        <div class="card card-outline-secondary my-4">
-          <div class="card-body" id="root">
-              You are not authorized to edit this post.
+          @if (Auth::user()->id == $post->organization->user->id)
+          @else
+          You are not authorized to edit this post.
           <a href="{{ route('posts.index') }}">Return to posts list.</a>
+          @endif
+
           </div>
         </div>
       </div>
     </div>
   </div>
-  @endif
-
+  
 @endsection
