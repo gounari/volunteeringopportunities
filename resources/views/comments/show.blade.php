fa3f54140ea4d61d15a305f789ee28d93da0b095
argyro <argyrogounari@gmail.com>
2019-12-19 20:21:04 +0200

    Refactor edit comments page so that only the author can edit

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+@if (Auth::user()->id === $comment->user_id)
 <div class="container">
     <div class="row">
       <div class="col-lg-9">
@@ -26,4 +27,19 @@
       </div>
     </div>
   </div>
+  @else
+    <div class="container">
+    <div class="row">
+      <div class="col-lg-9">
+        <div class="card card-outline-secondary my-4">
+          <div class="card-body" id="root">
+              You are not authorized to edit this comment.
+          <a href="{{ route('posts.index') }}">Return to posts list.</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  @endif
+
 @endsection
